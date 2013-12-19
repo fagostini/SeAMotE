b77d23e39aa4933e05c2215c9e8769a9358ade04
Federico Agostini <federico.agostini@crg.eu>
2013-12-19 15:14:43 +0100

    New changes

--- runseamote.sh
+++ runseamote.sh
@@ -14,8 +14,8 @@ cd   tmp/$1
 	./motifSearch -p positive.oneline $options -a rna
 
 	if [[ -s "tmp/best_motifs.dat" ]]; then
-		sed 's/R/[AG]/g;s/Y/[CT]/g;s/M/[CA]/g;s/K/[TG]/g;s/W/[TA]/g;s/S/[CG]/g;s/B/[CTG]/g;s/D/[ATG]/g;s/H/[ATC]/g;s/V/[ACG]/g' tmp/best_motifs.dat | awk '{print $1, $2, $4, $2/($2+$3), $4/($4+$5), $NF}' > outputs/best_motifs.txt
-		awk 'BEGIN{printf "<tbody>\n"}{printf "\t<tr>\n\t\t<td>%d</td>\n\t\t<td>%s</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%s</td>\n\t</tr>\n", NR, $1, $2, $3, $4*100, $5*100, sqrt(($4-$5)*($4-$5))*100, $6 }END{printf "</tbody>\n"}' ./outputs/best_motifs.txt > ./outputs/table.html
+		sed 's/R/[AG]/g;s/Y/[CT]/g;s/M/[CA]/g;s/K/[TG]/g;s/W/[TA]/g;s/S/[CG]/g;s/B/[CTG]/g;s/D/[ATG]/g;s/H/[ATC]/g;s/V/[ACG]/g' tmp/best_motifs.dat | awk '{print $1, $2, $4, $2/($2+$3), $4/($4+$5), $6, $NF}' > outputs/best_motifs.txt
+		awk 'BEGIN{printf "<tbody>\n"}{printf "\t<tr>\n\t\t<td>%d</td>\n\t\t<td>%s</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%E</td>\n\t</tr>\n", NR, $1, $2, $3, $4*100, $5*100, $6*100, $7 }END{printf "</tbody>\n"}' ./outputs/best_motifs.txt > ./outputs/table.html
 		mv tmp/*.dat outputs/
 	else
 		echo "The script has failed and no file has been created!"
---
+++ significant_new.R
@@ -0,0 +1,26 @@
+Args <- commandArgs()
+
+th <- as.double(Args[6])
+out <- as.integer(Args[7])
+
+DFall <- as.data.frame(read.table(paste("tmp/motifs_", Args[5],"nt.dat", sep="")))
+# DFdis <- as.data.frame(read.table(paste("tmp/motifs_", Args[5],"nt_Pdistribution.dat", sep="")))
+
+DF <- as.data.frame(DFall[which(DFall$V2/DFall$V3 >= th),])
+DF$V6 <- DF$V2/DF$V3-DF$V4/DF$V5
+DF$V3 <- DF$V3-DF$V2;
+DF$V5 <- DF$V5-DF$V4;
+DFbest <- DF[order(DF$V6, decreasing=T)[1:10],]
+
+myFun <- function(x){ m <- matrix(c(DFbest[x,]$V2, DFbest[x,]$V3, DFbest[x,]$V4, DFbest[x,]$V5), 2, 2); return(fisher.test(m, alternative="greater")$p.value) }
+Pval <- lapply(seq(1,10,1), myFun)
+DF$Pval <- unlist(Pval)
+
+# library(multicore)
+# myFun <- function(x){ m <- matrix(c(DF[x,]$V2, DF[x,]$V3, DF[x,]$V4, DF[x,]$V5), 2, 2); return(fisher.test(m, alternative="greater")$p.value) }
+# Pval <- mclapply(seq(1,dim(DF)[1],1), myFun, mc.cores=4)
+# DF$Pval <- unlist(Pval)
+
+write.table(DFbest,"tmp/best_motifs.dat", row.names=FALSE, col.names=FALSE, quote = FALSE, append=TRUE)
+# write.table(DF[order(unlist(Pval))[1:10],],"tmp/best_motifs.dat", row.names=FALSE, col.names=FALSE, quote = FALSE, append=TRUE)
+write.table(DF[which( (DF$V2/(DF$V2+DF$V3) >= th) ),], paste("tmp/motifs_",Args[5],"nt.dat", sep=""), row.names=FALSE, col.names=FALSE, quote = FALSE)
