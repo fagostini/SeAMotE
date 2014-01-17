316e0cc6b4db64f51e552a138375508df09813eb
Federico Agostini <federico.agostini@crg.eu>
2014-01-17 19:15:23 +0100

    inserted the loop for the logo creation

--- runseamote.sh
+++ runseamote.sh
@@ -24,6 +24,15 @@ cd   tmp/$1
 		awk '{print $1}' tmp/best_motifs.dat > iupac.txt
 		sed 's/R/[AG]/g;s/Y/[CT]/g;s/M/[CA]/g;s/K/[TG]/g;s/W/[TA]/g;s/S/[CG]/g;s/B/[CTG]/g;s/D/[ATG]/g;s/H/[ATC]/g;s/V/[ACG]/g' tmp/best_motifs.dat | awk '{print $1, $2, $4, $2/($2+$3), $4/($4+$5), $6, $NF}' > best_motifs.txt
 		paste iupac.txt best_motifs.txt > outputs/best_motifs.txt
+	
+		for((l=1;l<=`wc -l outputs/best_motifs.txt | awk '{print $1}'`;l++)); do
+			IUmot=`awk '(NR=='$l'){print $1}' outputs/best_motifs.txt`;
+			REmot=`awk '(NR=='$l'){print $2}' outputs/best_motifs.txt`;
+			grep -o -e "$REmot" outputs/positive.seq > matches.txt
+			python createLogo.py "$IUmot" 2&> python.log
+		done
+		rm matches.txt
+	
 		awk 'BEGIN{printf "<tbody>\n"}{printf "\t<tr>\n\t\t<td>%d</td>\n\t\t<td>%s</td>\n\t\t<td>%s</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%.3E</td>\n\t</tr>\n", NR, $1, $2, $3, $4, $5*100, $6*100, $7*100, $8 }END{printf "</tbody>\n"}' ./outputs/best_motifs.txt > ./outputs/table.html
 		mv tmp/*.dat outputs/
 	else
