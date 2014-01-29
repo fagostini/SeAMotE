6c0b8c6fa9815cb9bb8967d0db94392d02d2efe2
Federico Agostini <federico.agostini@crg.eu>
2014-01-29 15:28:52 +0100

    Few changes to the output page

--- template/createLogo.py
+++ template/createLogo.py
@@ -17,8 +17,8 @@ with open("logos/"+motif+"_pwm.txt", "w") as pwmFile:
 	for pos in range(len(m)):
 		print >> pwmFile, "%.3f %.3f %.3f %.3f" % (p[pos]["G"], p[pos]["A"], p[pos]["T"], p[pos]["C"])
 
-with open("logos/"+motif+"_transfac.txt", "w") as transFile:
-	print >> transFile, m.format("transfac")
+# with open("logos/"+motif+"_transfac.txt", "w") as transFile:
+# 	print >> transFile, m.format("transfac")
 
 m.weblogo("logos/"+motif+"_logo.png")
 
