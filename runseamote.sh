cb0418fcd1c2847e926f20145ccc5ed2bec5d68e
fagostini <processor@gians-beast.(none)>
2014-01-31 18:04:53 +0100

    inserted the motif size

--- runseamote.sh
+++ runseamote.sh
@@ -10,24 +10,24 @@ cd   tmp/$1
 			options="-r $3 "
 	fi
 	# Set the threshold and the molecule type
-	options+="-t $4 -a $5"  
+	options+="-t $4 -a $5 -w 7"
 
 	# Run the C script with the given parameters
 	./motifSearch -p positive.oneline $options
-	
+
 	mv positive.oneline outputs/positive.seq
 	if [[ -s "negative.oneline" ]]; then
 		mv negative.oneline outputs/reference.seq
 	else
 		mv reference.seq outputs/reference.seq
 	fi
-	
+
 	# Generate the output files and html tables
 	if [[ -s "tmp/best_motifs.dat" ]]; then
 		awk '{print $1}' tmp/best_motifs.dat > iupac.txt
 		sed 's/R/[AG]/g;s/Y/[CT]/g;s/M/[CA]/g;s/K/[TG]/g;s/W/[TA]/g;s/S/[CG]/g;s/B/[CTG]/g;s/D/[ATG]/g;s/H/[ATC]/g;s/V/[ACG]/g' tmp/best_motifs.dat | awk '{print $1, $2, $4, $2/($2+$3), $4/($4+$5), $6, $NF}' > best_motifs.txt
 		paste iupac.txt best_motifs.txt > outputs/best_motifs.txt
-		
+
 		# Generate the reverse complement for the DNA
 		mv outputs/positive.seq outputs/positive.tmp
 		if [[ "$5" == "dna" ]]; then
@@ -35,7 +35,7 @@ cd   tmp/$1
 		else
 			awk '{gsub(/U/, "T", $2); print }' outputs/positive.tmp > outputs/positive.seq
 		fi
-		
+
 		# Generate the logo and other files for each motif and the collected zip file
 		for((l=1;l<=`wc -l outputs/best_motifs.txt | awk '{print $1}'`;l++)); do
 			IUmot=`awk '(NR=='$l'){print $1}' outputs/best_motifs.txt`;
@@ -45,7 +45,7 @@ cd   tmp/$1
 		done
 		zip -r outputs/results.zip logos/*
 		rm matches.txt outputs/positive.tmp
-		
+
 		# Write the html files
 # 		awk 'BEGIN{printf "<tbody>\n"}{printf "\t<tr>\n\t\t<td>%d</td>\n\t\t<td>%s</td>\n\t\t<td>%s</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%.3E</td>\n\t</tr>\n", NR, $1, $2, $3, $4, $5*100, $6*100, $7*100, $8 }END{printf "</tbody>\n"}' ./outputs/best_motifs.txt > ./outputs/table.html
 # 		awk 'BEGIN{printf "<tbody>\n"}{printf "\t<tr>\n\t\t<td>%d</td>\n\t\t<td>%s</td>\n\t\t<td>%s</td>\n\t\t<td><img src=\"logos/%s_logo.png\" width=\"%d\" height=\"50\" style=\"solid white; display:block; margin-top:-1px; margin-left:1px; margin-right:-6px; margin-bottom:-1px;\"/></td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%d</td>\n\t\t<td>%.3E</td>\n\t</tr>\n", NR, $1, $2, $1, length($1)*30, $5*100, $6*100, $7*100, $8 }END{printf "</tbody>\n"}' ./outputs/best_motifs.txt > ./outputs/table.html
@@ -56,5 +56,5 @@ cd   tmp/$1
 		echo "The script has failed and no file has been created!"
 		exit 1
 	fi
-		
+
 cd ..
