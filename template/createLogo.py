18aa822254b62b1749a130113c326dfe08919fb3
Federico Agostini <federico.agostini@crg.eu>
2014-03-21 12:08:37 +0100

    bugfixes for the logos and p-values

--- template/createLogo.py
+++ template/createLogo.py
@@ -5,11 +5,16 @@ from Bio.Alphabet import IUPAC
 motif = sys.argv[1]
 
 m = Motif.Motif(alphabet=IUPAC.unambiguous_dna)
+ml = Motif.Motif(alphabet=IUPAC.unambiguous_dna)
 from Bio.Seq import Seq
 
+myLimit = 0
 with open("matches.txt", "r") as inFile:
 	for line in inFile:
 		m.add_instance(Seq(line[:-1], m.alphabet))
+		if myLimit < 35000:	
+			ml.add_instance(Seq(line[:-1], ml.alphabet))
+		myLimit += 1
 
 p = m.pwm()
 with open("logos/"+motif+"_pwm.txt", "w") as pwmFile:
@@ -19,6 +24,6 @@ with open("logos/"+motif+"_pwm.txt", "w") as pwmFile:
 
 # with open("logos/"+motif+"_transfac.txt", "w") as transFile:
 # 	print >> transFile, m.format("transfac")
-
-m.weblogo("logos/"+motif+"_logo.png")
+pl = ml.pwm()
+ml.weblogo("logos/"+motif+"_logo.png")
 
