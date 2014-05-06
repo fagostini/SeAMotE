0a21e59292cffbe768fb80c2385725926660e662
Federico Agostini <federico.agostini@crg.eu>
2014-05-06 16:17:54 +0200

    Modified some parts in the cross-validation script

--- run_cross-validation.sh
+++ run_cross-validation.sh
@@ -27,21 +27,83 @@
 # 
 # done
 
+# for d in `ls meme_seq/ | grep CV3.posi.seq`; do 
+# 	na=`echo "$d" | sed 's/\.CV3.posi.seq//g'`; 
+# 	echo "$na"
+# #  	sort -gk7 results/best_motifs_"$na".dat | head -5 | awk '{print $1}' > mt
+# #  	sort -gk7 results/best_motifs_"$na".dat | head -5 | awk '{for(i=1;i<=length($1);i++){if(i==1){printf ".%s\n", substr($1,i+1,length($1))} if(i==length($1)){printf "%s.\n", substr($1,1,length($1)-1)} if(i>1 && i<length($1)){printf "%s.%s\n", substr($1,1,i-1), substr($1,i,length($1)-i)}}}' > mt
+# 	sort -gk7 cross_validation_results/"$na".CV1CV3.txt | head -5 | awk '{print $1}' > mt
+# 	sort -gk7 cross_validation_results/"$na".CV1CV2.txt | head -5 | awk '{print $1}' >> mt
+# 	sort -gk7 cross_validation_results/"$na".CV3CV2.txt | head -5 | awk '{print $1}' >> mt
+# # 	sort -gk7 cross_validation_results/"$na".CV1.txt | head -6 | awk '{print $1}' >> mt
+# # 	sort -gk7 cross_validation_results/"$na".CV2.txt | head -6 | awk '{print $1}' >> mt
+# # 	sort -gk7 cross_validation_results/"$na".CV3.txt | head -6 | awk '{print $1}' >> mt
+# 	sort -u mt > mt2; mv mt2 mt
+# 	for j in `cat mt`; do
+# 		grep -w "$j" cross_validation_results/"$na".CV*CV* | awk 'END{printf "%d ", NR}';
+# # 		grep -w "$j" cross_validation_results/"$na".CV* | awk 'END{printf "%d ", NR}';
+# 	done
+# 	echo ""
+# done
+# | awk '($0 ~/ 2/ || $0 ~/ 3/)' | wc -l
+
+# | awk '($0 ~/ 4/ || $0 ~/ 5/ || $0 ~/ 6/)' | wc -l
+
+
+# for d in `ls meme_seq/ | grep CV3.posi.seq`; do 
+# 	na=`echo "$d" | sed 's/\.CV3.posi.seq//g'`; 
+# 	echo "$na"
+# 
+# 	sort -gk7 cross_validation_results/"$na".CV1CV2.txt | head -10 | awk '{print $1}' > mt
+# 	for j in `cat mt | awk '(length($1)>3)'`; do
+# 		grep -w "$j" cross_validation_results/"$na".CV3.txt | awk 'END{printf "%d ", NR}';
+# 	done
+# 	echo "A"
+# 	
+# 	sort -gk7 cross_validation_results/"$na".CV1CV3.txt | head -10 | awk '{print $1}' > mt
+# 	for j in `cat mt | awk '(length($1)>3)'`; do
+# 		grep -w "$j" cross_validation_results/"$na".CV2.txt | awk 'END{printf "%d ", NR}';
+# 	done
+# 	echo "B"
+# 	
+# 	sort -gk7 cross_validation_results/"$na".CV3CV2.txt | head -10 | awk '{print $1}' > mt
+# 	for j in `cat mt | awk '(length($1)>3)'`; do
+# 		grep -w "$j" cross_validation_results/"$na".CV1.txt | awk 'END{printf "%d ", NR}';
+# 	done
+# 	echo "C"
+# 	
+# done
+
+# USING TOMTOM
 for d in `ls meme_seq/ | grep CV3.posi.seq`; do 
 	na=`echo "$d" | sed 's/\.CV3.posi.seq//g'`; 
 	echo "$na"
-	sort -gk7 cross_validation_results/"$na".CV1CV3.txt | head -6 | awk '{print $1}' > mt
-	sort -gk7 cross_validation_results/"$na".CV1CV2.txt | head -6 | awk '{print $1}' >> mt
-	sort -gk7 cross_validation_results/"$na".CV3CV2.txt | head -6 | awk '{print $1}' >> mt
-# 	sort -gk7 cross_validation_results/"$na".CV1.txt | head -6 | awk '{print $1}' >> mt
-# 	sort -gk7 cross_validation_results/"$na".CV2.txt | head -6 | awk '{print $1}' >> mt
-# 	sort -gk7 cross_validation_results/"$na".CV3.txt | head -6 | awk '{print $1}' >> mt
-	sort -u mt > mt2; mv mt2 mt
-	for j in `cat mt`; do
-		grep -w "$j" cross_validation_results/"$na".CV*CV* | awk 'END{printf "%d ", NR}';
-# 		grep -w "$j" cross_validation_results/"$na".CV* | awk 'END{printf "%d ", NR}';
-	done
-	echo ""
-done | awk '($0 ~/ 2/ || $0 ~/ 3/)' | wc -l
 
-# | awk '($0 ~/ 4/ || $0 ~/ 5/ || $0 ~/ 6/)' | wc -l
+#	echo "CV1"
+	iupac2meme -alpha DNA -pseudo 1 `sort -gk7 cross_validation_results/"$na".CV1CV2.txt | head -3 | awk '{printf "%s ", $1}END{printf "\n"}'` > mt
+	iupac2meme -alpha DNA -pseudo 1 `sort -gk7 cross_validation_results/"$na".CV3.txt    | awk '{printf "%s ", $1}END{printf "\n"}'` > mf
+	tomtom -verbosity 1 -oc tomtom_out mt mf
+	head -3 tomtom_out/tomtom.txt | awk '($4<=0.05)'
+	wc -l cross_validation_results/"$na".CV1CV2.txt cross_validation_results/"$na".CV3.txt
+# 	for j in `head -3 tomtom_out/tomtom.txt | awk '($4<=0.05){print $1}'`; do grep -w "$j" cross_validation_results/"$na".CV1CV2.txt; done
+# 	for j in `head -3 tomtom_out/tomtom.txt | awk '($4<=0.05){print $2}'`; do grep -w "$j" cross_validation_results/"$na".CV3.txt; done
+		
+#	echo "CV2"	
+	iupac2meme -alpha DNA -pseudo 1 `sort -gk7 cross_validation_results/"$na".CV1CV3.txt | head -3 | awk '{printf "%s ", $1}END{printf "\n"}'` > mt
+	iupac2meme -alpha DNA -pseudo 1 `sort -gk7 cross_validation_results/"$na".CV2.txt    | awk '{printf "%s ", $1}END{printf "\n"}'` > mf
+	tomtom -verbosity 1 -oc tomtom_out mt mf
+	head -3 tomtom_out/tomtom.txt | awk '($4<=0.05)'
+	wc -l cross_validation_results/"$na".CV1CV3.txt cross_validation_results/"$na".CV2.txt
+# 	for j in `head -3 tomtom_out/tomtom.txt | awk '($4<=0.05){print $1}'`; do grep -w "$j" cross_validation_results/"$na".CV1CV3.txt; done
+# 	for j in `head -3 tomtom_out/tomtom.txt | awk '($4<=0.05){print $2}'`; do grep -w "$j" cross_validation_results/"$na".CV2.txt; done
+
+#	echo "CV3"
+	iupac2meme -alpha DNA -pseudo 1 `sort -gk7 cross_validation_results/"$na".CV3CV2.txt | head -3 | awk '{printf "%s ", $1}END{printf "\n"}'` > mt
+	iupac2meme -alpha DNA -pseudo 1 `sort -gk7 cross_validation_results/"$na".CV1.txt    | awk '{printf "%s ", $1}END{printf "\n"}'` > mf
+	tomtom -verbosity 1 -oc tomtom_out mt mf
+	head -3 tomtom_out/tomtom.txt | awk '($4<=0.05)'
+	wc -l cross_validation_results/"$na".CV3CV2.txt cross_validation_results/"$na".CV1.txt
+# 	for j in `head -3 tomtom_out/tomtom.txt | awk '($4<=0.05){print $1}'`; do grep -w "$j" cross_validation_results/"$na".CV3CV2.txt; done
+# 	for j in `head -3 tomtom_out/tomtom.txt | awk '($4<=0.05){print $2}'`; do grep -w "$j" cross_validation_results/"$na".CV1.txt; done
+	
+done
